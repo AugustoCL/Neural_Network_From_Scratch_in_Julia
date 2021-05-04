4c5cab5d27b12db8ddf1c289ccbfcd1a9f824808
AugustoCL <augustoleal72@gmail.com>
2021-05-04 15:26:17 -0300

    update chapter2 and add column orientation in layer struct

--- Chapters/Chapter02.jl
+++ Chapters/Chapter02.jl
@@ -1,8 +1,11 @@
 ### A Pluto.jl notebook ###
-# v0.14.2
+# v0.14.5
 
 using Markdown
 using InteractiveUtils
+
+# ╔═╡ 9522185e-2666-4cf9-a228-a9ed2f1fd4f5
+using LinearAlgebra
 
 # ╔═╡ 0a5335e0-ab9e-11eb-0a85-139ff5cf0b1d
 md"# Chapter 02 from NNFS book"
@@ -12,7 +15,7 @@ md"### A Single Neuron (4 inputs, 1 output)"
 
 # ╔═╡ d85b097d-c7c7-439f-80ec-a1b75c696cb1
 md""" 
-for **each input** we need **one weigth** and for **each output** we need **one bias**.
+For **each input** we need **one weigth** and for **each output** we need **one bias**.
 So, for this simple example with 1 neuron with 4 inputs we are gonna have:
 
 - **3** inputs
@@ -20,13 +23,41 @@ So, for this simple example with 1 neuron with 4 inputs we are gonna have:
 - **1** bias.
 """
 
+# ╔═╡ abb7a21d-9048-4d09-8b41-cdd519773756
+md"""
+#### Introduction the notation
+The output of the neuron $$i$$, $$a^{(L)}_i$$, is equal to the scalar product of weights vector $$W_i$$ by the input vector $$a^{(L-1)}$$, plus bias $$b_i$$.\
+"""
+
+# ╔═╡ 73f22e47-8671-44a9-ab74-786b281b9d69
+md"""
+$$a_i^{(L)} = W_i \cdot a^{(L - 1)} + b_i$$
+"""
+
+# ╔═╡ 0ebd7094-37ab-4151-b874-58b6347eb0d9
+md"""
+For a number $$j$$ of inputs and a number $$k$$ from outputs, each weight from $$k$$ must be in a line in $$W$$. This way, the column vector of outputs is equal to matrix multiplication of weights by the column vector inputs plus bias.\
+"""
+
+# ╔═╡ 37bbfcde-9c52-436a-96ef-bacf158ef5bd
+md"""
+$$a_{k × 1}^{(L)} = W_{k × j}×a_{j × 1}^{(L - 1)} + b_{k × 1}$$
+"""
+
+# ╔═╡ 280badb4-b649-4805-b84d-b18797555c2b
+md"""
+Applying that notation in our first layer example of 4 inputs and 1 output we got:
+
+$$a_{1 × 1}^{(1)} = W_{1 × 4}×a_{4 × 1}^{(0)} + b_{1 × 1}$$
+"""
+
 # ╔═╡ ef746509-f29b-42ae-b9ec-a2e18ad4b327
 begin
-	inputs = [1; 2; 3; 2.5]
-	weights = [0.2; 0.8; -0.5; 1.0]
+	inputs = [1, 2, 3, 2.5]
+	weights = [0.2, 0.8, -0.5, 1.0]
 	bias = 2.0
 
-	inputs'*weights + bias # or dot(inputs, weights) + bias
+	weights ⋅ inputs + bias # \cdot or dot(w, i)
 end
 
 # ╔═╡ a3acafe8-6a4b-4438-84d9-16a00cb21997
@@ -42,52 +73,107 @@ begin
 	bias2 = 3.0
 	bias3 = 0.5
 
-	[inputs'*weights1 + bias1,
-	inputs'*weights2 + bias2,
-	inputs'*weights3 + bias3]
+	[weights1 ⋅ inputs + bias1,
+	 weights2 ⋅ inputs + bias2,
+	 weights3 ⋅ inputs + bias3]
 end
 
+# ╔═╡ d96ad6e1-65de-4150-8fdd-579b8653dd39
+md"""
+Applying the notation in that layer with 4 inputs and 3 output we got:
+
+$$a_{3 × 1}^{(1)} = W_{3 × 4}×a_{4 × 1}^{(0)} + b_{3 × 1}$$
+"""
+
 # ╔═╡ 3613059e-501d-4cb0-bd73-8e42e6395e16
-md" with only one unique matrix of weights we use matrix multiplication from linear algebra"
+md" With only one unique matrix of weights we use matrix multiplication."
 
 # ╔═╡ 0e309e2a-59e8-4fce-819d-afeec8c21ca8
 begin
-	weights_array = [
-		0.2    0.8  -0.5   1.0;
-		0.5   -0.91  0.26 -0.5;
-		-0.26 -0.27  0.17  0.87;
-	]
-	bias_vec = [2.0 3.0 0.5]
+	weights_matrix = [
+		 0.2   0.8  -0.5   1.0
+		 0.5  -0.91  0.26 -0.5
+		-0.26 -0.27  0.17  0.87
+		]
+					  
+	bias_vec = [2.0, 3.0, 0.5]
 	
-	(weights_array * inputs) .+ bias_vec
+	weights_matrix * inputs + bias_vec
 end
 
 # ╔═╡ d27568ac-934c-4408-880d-4bd34ccead72
 md"### A Layer of Neurons with multiple batch of Data"
 
 # ╔═╡ 91d6b3e6-5124-4963-b6c2-f2c65b67b045
-md"Now we are gonna need use transpose operation to match the right vectors in matrix multiplication"
+md"""
+When we evaluate more than one observation (a batch of data), we'll have a matrix in the input $$a_{j × n}$$, with $$n$$ as the number of observations. 
+This way we could update the notation to:
+
+$$a_{k × n}^{(L)} = W_{k × j}×a_{j × n}^{(L - 1)} + b_{k × n}$$
+"""
+
+# ╔═╡ 56cfbb4c-6573-485c-ac2a-6d66941ffbf8
+md"""
+#### About the arquitecture of a layer.
+We could construct the layer by two ways: 
+- Observations in Row and Parameters in Column (row orientation)
+- Observations in Column and Parameters in Row. (column orientation)
+
+This happens because languages have differents orientation. In Python which is row orientation, all the code is constructed by the first way, but in other languages like Julia, or even the math textbooks, you could use the column orientation.
+"""
+
+# ╔═╡ 277b4bcc-635e-48b2-b030-b51f159f3eb3
+md"""
+The **first one** is like the code above.
+"""
+
+# ╔═╡ 5a066946-e2f0-48c7-9b4b-6089caca52fb
+begin
+	inputs_batch01 = [
+		 1.0 2.0  3.0  2.5
+		 2.0 5.0 -1.0  2.0
+		-1.5 2.7  3.3 -0.8
+	]
+	#bias_vec = [2.0, 3.0, 0.5]
+	(input01 = inputs_batch01,
+	 output01 = (inputs_batch01 * weights_matrix') .+ bias_vec')
+end
+
+# ╔═╡ bba52778-115b-4e0d-b63b-2b1f4802736f
+md"""
+The **second one** is like the code above.
+"""
 
 # ╔═╡ 7ae39d13-e448-4c91-81be-d44a15f1a414
 begin
-	inputs_batch = [
-		1.0 2.0 3.0 2.5
-		2.0 5.0 -1.0 2.0
-		-1.5 2.7 3.3 -0.8
+	inputs_batch02 = [
+		[1.0, 2.0, 3.0, 2.5] [2.0, 5.0, -1.0, 2.0] [-1.5, 2.7, 3.3, -0.8]
 	]
-	
-	(inputs_batch * weights_array') .+ bias_vec 
+	#bias_vec = [2.0, 3.0, 0.5]
+	(input02 = inputs_batch02,
+	 output02 = (weights_matrix * inputs_batch02) .+ bias_vec)
 end
 
 # ╔═╡ Cell order:
 # ╟─0a5335e0-ab9e-11eb-0a85-139ff5cf0b1d
 # ╟─0c5d667f-0ea5-4d21-80ad-58d144470a73
 # ╟─d85b097d-c7c7-439f-80ec-a1b75c696cb1
+# ╟─abb7a21d-9048-4d09-8b41-cdd519773756
+# ╟─73f22e47-8671-44a9-ab74-786b281b9d69
+# ╟─0ebd7094-37ab-4151-b874-58b6347eb0d9
+# ╟─37bbfcde-9c52-436a-96ef-bacf158ef5bd
+# ╟─280badb4-b649-4805-b84d-b18797555c2b
+# ╠═9522185e-2666-4cf9-a228-a9ed2f1fd4f5
 # ╠═ef746509-f29b-42ae-b9ec-a2e18ad4b327
 # ╟─a3acafe8-6a4b-4438-84d9-16a00cb21997
 # ╠═9fbc1e4c-52c0-4b99-92dc-4969b55f645d
+# ╟─d96ad6e1-65de-4150-8fdd-579b8653dd39
 # ╟─3613059e-501d-4cb0-bd73-8e42e6395e16
 # ╠═0e309e2a-59e8-4fce-819d-afeec8c21ca8
 # ╟─d27568ac-934c-4408-880d-4bd34ccead72
 # ╟─91d6b3e6-5124-4963-b6c2-f2c65b67b045
+# ╟─56cfbb4c-6573-485c-ac2a-6d66941ffbf8
+# ╟─277b4bcc-635e-48b2-b030-b51f159f3eb3
+# ╠═5a066946-e2f0-48c7-9b4b-6089caca52fb
+# ╟─bba52778-115b-4e0d-b63b-2b1f4802736f
 # ╠═7ae39d13-e448-4c91-81be-d44a15f1a414
--- Layer_dense.jl
+++ Layer_dense.jl
@@ -4,25 +4,41 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ c4ea1432-abb4-11eb-1cf5-edac0735d67d
-md"# Layer_dense"
-
 # ╔═╡ be7baa4f-6871-4d74-b4d2-1c2534cdfa17
 begin
+	using Random
+	Random.seed!(0)
+	
 	struct Layer_dense
 		w::Matrix{Float64}
 		b::Matrix{Float64}
 		σ::Function
 	
 		function Layer_dense(n_in::Int, n_out::Int; σ::Function = identity)
-			w = 0.01 .* randn(n_in, n_out)
+			w = 0.01 .* randn(n_out, n_in)
 			b = reshape(zeros(n_out), (1, n_out)) 
 			return new(w, b, σ)
 		end
 	end
 
-	(layer::Layer_dense)(input) = layer.σ.( (input * layer.w) .+ layer.b )
+	function (L::Layer_dense)(input) 
+		W, b, σ = L.w, L.b, L.σ
+		σ.( (input * W') .+ b )
+	end
 end
+
+# ╔═╡ c4ea1432-abb4-11eb-1cf5-edac0735d67d
+md"# Layer_dense"
+
+# ╔═╡ 45d31083-883b-4e84-913e-c040c6fdfd5f
+md"""
+>No futuro, podemos pensar em novas formas de fazer esse objeto. Por exemplo, passando a matriz W como input construtor do layer.
+"""
+
+# ╔═╡ e22e7c1c-e1c4-4ae3-96c1-9cbd71fcc999
+md"""
+### Row Orientation
+"""
 
 # ╔═╡ 58265283-a36a-4c83-9101-3387165da021
 begin
@@ -35,17 +51,59 @@ end
 D = Layer_dense(2,3, σ = logistic) # sigmoids: tanh, atan, ReLU, logistic, softplus 
 
 # ╔═╡ 00c04f4e-e883-46af-a51c-9fd5f227e685
-D([1 4; 2 3])
+D([1 4; 2 3; 3 5])
 
-# ╔═╡ 45d31083-883b-4e84-913e-c040c6fdfd5f
+# ╔═╡ dc76be1b-292e-4545-887e-1e62faf8e1b6
 md"""
-No futuro, podemos pensar em novas formas de fazer esse objeto. Por exemplo, passando a matriz W como input construtor do layer.
+### Column Orientation
 """
+
+# ╔═╡ fe237f5f-8f90-429a-8ddc-e5933b8fd808
+begin
+	Random.seed!(0)
+	
+	struct Layer_dense_v02
+		w::Matrix{Float64}
+		b::Vector{Float64}
+		σ::Function
+	
+		function Layer_dense_v02(n_in::Int, n_out::Int; σ::Function = identity)
+			w = 0.01 .* randn(n_out, n_in)
+			b = zeros(n_out) 
+			return new(w, b, σ)
+		end
+	end
+
+	function (L::Layer_dense_v02)(input)
+		W, b, σ = L.w, L.b, L.σ
+		σ.( (W * input) .+ b' )
+	end
+end
+
+# ╔═╡ d0c91a1c-8798-4985-978b-4c47d1abc212
+E = Layer_dense_v02(2, 3, σ = logistic)
+
+# ╔═╡ 3de0841e-f0f3-436c-a891-dd4af32d2af6
+#E(randn(4,3))
+E([1 4; 2 3; 3 5]')
+
+# ╔═╡ 92661b11-5e13-4c8c-918b-1b85f49d583d
+(
+	by_row = D([1 4; 2 3; 3 5]), 
+	# para a orientacao coluna, o input precisa ser transposto
+	by_col = E([1 4; 2 3; 3 5]') 
+)
 
 # ╔═╡ Cell order:
 # ╟─c4ea1432-abb4-11eb-1cf5-edac0735d67d
+# ╟─45d31083-883b-4e84-913e-c040c6fdfd5f
+# ╟─e22e7c1c-e1c4-4ae3-96c1-9cbd71fcc999
 # ╠═be7baa4f-6871-4d74-b4d2-1c2534cdfa17
 # ╠═58265283-a36a-4c83-9101-3387165da021
 # ╠═d904c2ae-e035-40be-b46b-79ffd500f284
 # ╠═00c04f4e-e883-46af-a51c-9fd5f227e685
-# ╟─45d31083-883b-4e84-913e-c040c6fdfd5f
+# ╟─dc76be1b-292e-4545-887e-1e62faf8e1b6
+# ╠═fe237f5f-8f90-429a-8ddc-e5933b8fd808
+# ╠═d0c91a1c-8798-4985-978b-4c47d1abc212
+# ╠═3de0841e-f0f3-436c-a891-dd4af32d2af6
+# ╠═92661b11-5e13-4c8c-918b-1b85f49d583d
