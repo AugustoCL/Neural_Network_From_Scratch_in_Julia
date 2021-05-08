18d7cfcbbd100eadd3035f019f9fbb53bad877ce
AugustoCL <augustoleal72@gmail.com>
2021-05-08 01:06:11 -0300

    update structs and sigmoid functions

--- Layer_dense.jl
+++ Layer_dense.jl
@@ -66,12 +66,12 @@ begin
 		return LayerDense(W, b, σ)
 	end
 	
-	function (L::LayerDense)(input::Vector)
+	function (L::LayerDense)(input::Vector{<:Real})
 		W, b, σ = L.W, L.b, L.σ
 		σ(W * input + b)
 	end
 	
-	function (L::LayerDense)(input::Matrix)
+	function (L::LayerDense)(input::Matrix{<:Real})
 		W, b, σ = L.W, L.b, L.σ
 		σ(W * input' .+ b)
 	end
@@ -98,30 +98,36 @@ B = LayerDense(4, 3)
 
 # ╔═╡ 142bb8f9-4188-46d2-94a2-8407d633c496
 input = [2 3 4 5
-		 5 3 5 7] # cada obeservação deve estar em uma linha.
+		 5 3 5 7] # cada observação deve estar em uma linha.
 
 # ╔═╡ 2377f014-0bd0-457b-b9e9-3ace1ad7c331
-B(input) #cada coluna é um output
+B(input) # cada coluna é um output
 
 # ╔═╡ c99c7f4f-7e62-4036-ba08-4478e5480d19
 md"### Adding new sigmoid functions"
 
 # ╔═╡ 58265283-a36a-4c83-9101-3387165da021
 begin
-	logistic(x::Real) = 1 / (1 + ℯ^(-x))
-	logistic(x::AbstractVecOrMat{T}) where {T<:Float64} = logistic.(x)
+	function σ(x::Real) 
+		t = exp(-abs(x))
+		return ifelse(x ≥ 0, inv(1 + t), t / (1 + t))
+	end	
+	
+	σ(x::AbstractVecOrMat{T}) where {T<:Real} = σ.(x)
+	
+	const sigmoid = σ
 end
 
 # ╔═╡ 45bafada-34ec-4c41-b0a2-000f35ebffef
 begin
-	ReLU(x::Real) = max(0, x)
-	ReLU(x::AbstractVecOrMat{T}) where {T<:Float64} = ReLU.(x)
+	relu(x::Real) = max(zero(x), x) # zero function keep the type of x
+	relu(x::AbstractVecOrMat{T}) where {T<:Real} = relu.(x)
 end
 
 # ╔═╡ d6fce490-1498-469d-8ecc-55d0a71d1565
 begin
-	softplus(x::Real) = log((1+ℯ^x), ℯ)
-	softplus(x::AbstractVecOrMat{T}) where {T<:Float64} = softplus.(x)
+	softplus(x::Real) = log((1+ℯ^x))
+	softplus(x::AbstractVecOrMat{T}) where {T<:Real} = softplus.(x)
 end
 
 # ╔═╡ 04cadf6a-4fd1-4b5f-aa4d-140ea6e5cff2
@@ -129,22 +135,22 @@ function softmax(x::Vector{T}) where {T<:Real}
 	m = maximum(x)
 	exp_val = exp.(x .- m)
 	s = sum(exp_val)
-	exp_val ./ s
+	return exp_val ./ s
 end
 
 # ╔═╡ a7898ffd-1acb-4220-bbc8-3b428b95db85
 function softmax(x::Matrix{T}) where {T<:Real}
 	m = maximum.(eachcol(x))
-	exp_val = exp.(x .- m')	
+	exp_val = exp.(x .- m')
 	s = sum(eachrow(exp_val))
-	return exp_val ./ s' 
+	return exp_val ./ s'
 end
 
 # ╔═╡ c116a1e4-fd4d-446f-bdea-02301f43b2c0
 md"**Aplying some sigmoid functions**"
 
 # ╔═╡ d0c91a1c-8798-4985-978b-4c47d1abc212
-C = LayerDense(2, 3, logistic)
+C = LayerDense(2, 3, σ)
 
 # ╔═╡ 3de0841e-f0f3-436c-a891-dd4af32d2af6
 #E(randn(4,3))
@@ -152,6 +158,44 @@ C([1 4; 2 3; 3 5])
 
 # ╔═╡ 963f6276-2440-4bfe-ae0f-539d8bfae0a2
 D = LayerDense(2, 3, softmax)
+
+# ╔═╡ 1499b700-960a-4075-aa89-f5780be39728
+begin
+	function CE(outp, target)
+		R = []
+		for (i,d) in zip(target, eachcol(outp))
+			push!(R, d[i])
+		end
+		return R
+	end
+
+	outp = D([1 4; 2 3; 3 5])
+	target = [1, 2, 2]
+	R = []
+	for (i,d) in zip(target, eachcol(outp))
+		push!(R, d[i])
+	end
+
+	(outp, R)
+end
+
+# ╔═╡ e3ec0333-14ad-42a8-9cd3-4e3937dc5ff0
+begin
+	outp1 = [
+		0.7 0.1 0.2
+		0.1 0.5 0.4
+		0.02 0.9 0.08
+	]
+	target1 = [
+		1 0 0
+		0 1 0
+		0 1 0
+	]
+	
+	#( outp1, target1')
+	outp1*target1'
+	#sum(outp1*target1', dims=2)
+end
 
 # ╔═╡ f4b146c1-af4a-4c3e-860d-1f92c4c13a6e
 ( D([1 4; 2 3; 3 5]), sum(eachrow(D([1 4; 2 3; 3 5]))) )
@@ -179,5 +223,7 @@ D = LayerDense(2, 3, softmax)
 # ╠═d0c91a1c-8798-4985-978b-4c47d1abc212
 # ╠═3de0841e-f0f3-436c-a891-dd4af32d2af6
 # ╠═963f6276-2440-4bfe-ae0f-539d8bfae0a2
+# ╠═1499b700-960a-4075-aa89-f5780be39728
+# ╠═e3ec0333-14ad-42a8-9cd3-4e3937dc5ff0
 # ╠═f4b146c1-af4a-4c3e-860d-1f92c4c13a6e
 # ╟─be7baa4f-6871-4d74-b4d2-1c2534cdfa17
